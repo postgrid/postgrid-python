6681008b6a72d781f01a534b0540b7a6414d832b
Apaar Madan <apaar_madan@hotmail.com>
2022-03-11 15:47:07 -0500

    Add support for POST-ing files

--- postgrid/__init__.py
+++ postgrid/__init__.py
@@ -6,6 +6,11 @@ av_base = 'https://api.postgrid/com/v1/'
 pm_key = None
 av_key = None
 
+MAX_LIMIT=100
+
+def _is_file_like(f):
+    return hasattr(f, 'read')
+
 def _camel_to_snake(s):
     new_s = ''
 
@@ -17,9 +22,23 @@ def _camel_to_snake(s):
 
     return new_s
 
+def _snake_to_camel(s):
+    new_s = ''
+
+    for i, ch in enumerate(s):
+        if s[i] == '_':
+            continue
+
+        if i > 0 and s[i - 1] == '_':
+            new_s += ch.upper()
+        else:
+            new_s += ch
+        
+    return new_s
+
 def _camel_to_snake_dict_keys_recursive(d):
     if isinstance(d, list):
-        return map(_camel_to_snake_dict_keys_recursive, d)
+        return list(map(_camel_to_snake_dict_keys_recursive, d))
     elif not isinstance(d, dict):
         return d
 
@@ -31,20 +50,57 @@ def _camel_to_snake_dict_keys_recursive(d):
     return new_d
 
 def _request(base, endpoint, method='GET', **kwargs):
+    assert base in [pm_base, av_base]
+    
     headers = {'x-api-key': pm_key if base == pm_base else av_key}
 
     if method == 'GET':
         res = requests.get(base + endpoint, params=kwargs, headers=headers)
     elif method == 'POST':
-        res = requests.post(base + endpoint, json=kwargs, headers=headers)
+        data = []
+        files = None
+
+        def flatten(key, value):
+            if _is_file_like(value):
+                if not files:
+                    files = {}
+
+                files[key] = value
+            elif isinstance(value, dict):
+                for inner_key, inner_value in value.items():
+                    flatten(f'{key}[{inner_key}]', inner_value)
+            elif isinstance(value, list):
+                for inner_value in value:
+                    flatten(f'{key}[]', inner_value)
+            elif value is not None:
+                data.append((key, value))
+
+        for key, value in kwargs.items():
+            flatten(key, value)
+        
+        res = requests.post(base + endpoint, data=data, files=files, headers=headers)
     elif method == 'DELETE':
         res = requests.delete(base + endpoint, params=kwargs, headers=headers)
     else:
         raise NotImplementedError()
 
-    res.raise_for_status()
+    value = res.json()
 
-    return _convert_json_value(res.json())
+    try:
+        res.raise_for_status()
+    except requests.HTTPError as e:
+        if base == pm_base:
+            raise PMError(status_code=res.status_code,
+                          object=value['object'],
+                          type=value['error']['type'],
+                          message=value['error']['message'])
+        else:
+            raise NotImplementedError()
+
+    if base == pm_base:
+        return _pm_convert_json_value(value)
+    else:
+        raise NotImplementedError()
 
 def _pm_get(endpoint, **kwargs):
     return _request(pm_base, endpoint, method='GET', **kwargs)
@@ -52,52 +108,127 @@ def _pm_get(endpoint, **kwargs):
 def _pm_post(endpoint, **kwargs):
     return _request(pm_base, endpoint, method='POST', **kwargs)
 
+def _pm_delete(endpoint, **kwargs):
+    return _request(pm_base, endpoint, method='DELETE', **kwargs)
+
+class PMError(Exception):
+    def __init__(self, status_code, object, type, message):
+        super().__init__(message)
+
+        self.status_code = status_code
+        self.object = object
+        self.type = type
+        self.message = message
+
 class PMResource:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
+    @classmethod
+    def _create(cls, locals_):
+        assert 'cls' in locals_
+        assert 'kwargs' in locals_
+
+        locals_except_kwargs_and_cls = {
+            _snake_to_camel(key): value for key, value in locals_.items() if key != 'kwargs' and key != 'cls'
+        }
+
+        return _pm_post(cls.endpoint, **locals_except_kwargs_and_cls, **locals_['kwargs'])
+
+    @classmethod
+    def list(cls, skip=0, limit=10):
+        return _pm_get('contacts', skip=skip, limit=limit)
     
+    @classmethod
+    def list_autopaginate(cls, max=None):
+        # We call the derived class's list method repeatedly
+        
+        # FIXME: Wasteful because we request MAX_LIMIT always and then potentially discard
+        # a certain number of contacts.
+        data = []
+        last_total_count = None
+
+        while True:
+            list_ = cls.list(skip=len(data),
+                             limit=MAX_LIMIT)
+
+            data.extend(list_.data)
+            last_total_count = list_.total_count
+
+            if len(data) >= list_.total_count or \
+               (max and len(data) >= max):
+                break
+        
+        if max:
+            data = data[:max]
+
+        return List(
+            object='list',
+            total_count=last_total_count,
+            skip=0,
+            limit=max if max else len(data),
+            data=data
+        )
+
+    @classmethod
+    def retrieve(cls, id):
+        return _pm_get(f'{cls.endpoint}/{id}')
+
+    @classmethod
+    def delete(cls, id):
+        return _pm_delete(f'{cls.endpoint}/{id}')
+ 
     def to_dict(self):
         return vars(self)
 
 class List(PMResource):
-    def __init__(self, total_count, skip, limit, data):
+    def __init__(self, object, total_count, skip, limit, data):
+        self.object = object
         self.total_count = total_count
         self.skip = skip
         self.limit = limit
-        self.data = map(_convert_json_value, data)
+
+        if len(data) > 0 and isinstance(data[0], dict):
+            data = list(map(_pm_convert_json_value, data))
+
+        self.data = data
 
 class Contact(PMResource):
-    @staticmethod
-    def list(skip, limit):
-        return _pm_get('contacts', skip, limit)
+    endpoint = 'contacts'
 
-    @staticmethod
-    def retrieve(id):
-        return _pm_get('contacts/' + id)
-
-    @staticmethod
-    def create(address_line1, country_code, first_name=None, last_name=None,
+    @classmethod
+    def create(cls, address_line1, country_code, first_name=None, last_name=None,
                company_name=None, address_line2=None, city=None,
                province_or_state=None, email=None, phone_number=None,
                job_title=None, postal_or_zip=None, **kwargs):
-        return _pm_post('contacts', 
-                 addressLine1=address_line1,
-                 addressLine2=address_line2,
-                 city=city,
-                 provinceOrState=province_or_state,
-                 postalOrZip=postal_or_zip,
-                 countryCode=country_code,
-                 firstName=first_name,
-                 lastName=last_name,
-                 companyName=company_name,
-                 email=email,
-                 phoneNumber=phone_number,
-                 jobTitle=job_title,
-                 **kwargs)
+        return cls._create(locals())
 
-OBJECT_TO_CLASS = {
-    'contact': Contact
+class Template(PMResource):
+    endpoint = 'templates'
+
+    @classmethod
+    def create(cls, html, **kwargs):
+        return cls._create(locals())
+
+class BankAccount(PMResource):
+    endpoint = 'bank_accounts'
+
+    @classmethod
+    def create(cls, bank_name, account_number, bank_primary_line,
+                    bank_country_code, transit_number=None, route_number=None, 
+                    routing_number=None, signature_image=None, signature_text=None,
+                    bank_secondary_line=None):
+        cls._create(locals())
+
+PM_OBJECT_TO_CLASS = {
+    'contact': Contact,
+    'template': Template,
+    'list': List
 }
 
-def _convert_json_value(value):
-    return OBJECT_TO_CLASS[value['object']](**_camel_to_snake_dict_keys_recursive(value))
+def _pm_convert_json_value(value):
+    for key, inner_value in value.items():
+        if isinstance(inner_value, dict) and 'object' in inner_value:
+            value[_camel_to_snake(key)] = _pm_convert_json_value(inner_value)
+
+    return PM_OBJECT_TO_CLASS[value['object']](**_camel_to_snake_dict_keys_recursive(value))
