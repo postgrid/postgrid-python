ecd8ad40f9aaa390d4609c3aa5a5cd7a3591270b
Apaar Madan <apaar_madan@hotmail.com>
2022-06-06 16:24:16 -0400

    Update version to 1.0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / 'README.md').read_text()
 
 setuptools.setup(
     name='postgrid-python',
-    version='1.0.5',
+    version='1.0.6',
     description='A Python Library for the PostGrid Print & Mail API',
     long_description=README,
     long_description_content_type='text/markdown',
