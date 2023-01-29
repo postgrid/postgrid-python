4f189b714d3a4ec153cdd9a1df9a2361f4cbe8eb
Jeffrey Lin <jeffreylin39@gmail.com>
2023-01-29 18:20:06 -0500

    ENG-173 Added test for templates (#8)
    * added test for templates
    
    * Fixed retrieve test
    
    * ran autoformatter

---
+++ tests/test_template.py
@@ -0,0 +1,66 @@
+import postgrid
+import os
+
+
+def setup_module():
+    postgrid.pm_key = os.environ.get('PM_API_KEY')
+
+
+def create_test_template():
+    return postgrid.Template.create(
+        html='<b>test</b> template, hello {{to.firstName}}!',
+        description='Test',
+        metadata={
+            'test': [10, 20]
+        }
+    )
+
+
+def test_create():
+    template = create_test_template()
+
+    assert isinstance(template, postgrid.Template)
+    assert isinstance(template.html, str)
+    assert isinstance(template.description, str)
+    assert template.html == '<b>test</b> template, hello {{to.firstName}}!'
+    assert template.description == 'Test'
+
+
+def test_retrieve():
+    template = create_test_template()
+    assert isinstance(template, postgrid.Template)
+
+    template = postgrid.Template.retrieve(template.id)
+    assert isinstance(template, postgrid.Template)
+
+
+def test_list():
+    template = create_test_template()
+    list_ = postgrid.Template.list()
+
+    assert list_.total_count >= 1
+    assert list_.skip == 0
+    assert isinstance(list_.data[0], postgrid.Template)
+    assert list_.data[0].html == template.html
+    assert list_.data[0].description == template.description
+
+
+def test_update():
+    template = create_test_template()
+    assert isinstance(template, postgrid.Template)
+
+    template = postgrid.Template.update(
+        template.id, html='updated template', description='updated description')
+    assert isinstance(template, postgrid.Template)
+    assert template.html == 'updated template'
+    assert template.description == 'updated description'
+
+
+def test_delete():
+    template = create_test_template()
+    assert isinstance(template, postgrid.Template)
+
+    res = postgrid.Template.delete(template.id)
+
+    assert isinstance(res, postgrid.Template)
+    assert res.deleted == True
